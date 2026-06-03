3b3b6643639fc3fad20d06bcdf28a71c26c9d097
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2026-06-03 20:57:19 +1000

    just updating

---
+++ test_cpp/sample.cpp
@@ -0,0 +1,15 @@
+/*
+This is a simple C++ program.
+Call this file sample.cpp.
+*/
+
+#include <iostream>
+using namespace std; 
+
+// A C++ program begins at main().
+int main()
+{
+    cout << "C++ is power programming.";
+
+    return 0;
+}
